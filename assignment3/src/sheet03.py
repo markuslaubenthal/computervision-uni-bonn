61f0c21d7a2d8271fefe37c5b1bdf8ea72b9210b
Markus Laubenthal <mail@markuslaubenthal.de>
2019-11-02 12:36:01 +0100

    1a + kmeans init

---
+++ assignment3/src/sheet03.py
@@ -0,0 +1,194 @@
+import numpy as np
+import cv2 as cv
+import random
+
+
+def display(img):
+    cv.imshow('image', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+##############################################
+#     Task 1        ##########################
+##############################################
+
+
+def task_1_a():
+    print("Task 1 (a) ...")
+    img = cv.imread('../images/shapes.png')
+    # img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    '''
+    ...
+    your code ...
+    ...
+    '''
+    edges = cv.Canny(img,50,150,apertureSize = 3)
+    # display(edges)
+    houghLines = cv.HoughLines(edges,0.7,np.pi/180,55)
+    for x in houghLines:
+        rho,theta = x[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+    display(img)
+
+def myHoughLines(img_edges, d_resolution, theta_step_sz, threshold):
+    """
+    Your implementation of HoughLines
+    :param img_edges: single-channel binary source image (e.g: edges)
+    :param d_resolution: the resolution for the distance parameter
+    :param theta_step_sz: the resolution for the angle parameter
+    :param threshold: minimum number of votes to consider a detection
+    :return: list of detected lines as (d, theta) pairs and the accumulator
+    """
+    accumulator = np.zeros((int(180 / theta_step_sz), int(np.linalg.norm(img_edges.shape) / d_resolution)))
+    detected_lines = []
+    '''
+    ...
+    your code ...
+    ...
+    '''
+    return detected_lines, accumulator
+
+
+def task_1_b():
+    print("Task 1 (b) ...")
+    img = cv.imread('../images/shapes.png')
+    img_gray = None # convert the image into grayscale
+    edges = None # detect the edges
+    #detected_lines, accumulator = myHoughLines(edges, 1, 2, 50)
+    '''
+    ...
+    your code ...
+    ...
+    '''
+
+
+##############################################
+#     Task 2        ##########################
+##############################################
+
+
+def task_2():
+    print("Task 2 ...")
+    img = cv.imread('../images/line.png')
+    img_gray = None # convert the image into grayscale
+    edges = None # detect the edges
+    theta_res = None # set the resolution of theta
+    d_res = None # set the distance resolution
+    #_, accumulator = myHoughLines(edges, d_res, theta_res, 50)
+    '''
+    ...
+    your code ...
+    ...
+    '''
+
+
+##############################################
+#     Task 3        ##########################
+##############################################
+
+
+def myKmeans(data, k):
+    """
+    Your implementation of k-means algorithm
+    :param data: list of data points to cluster
+    :param k: number of clusters
+    :return: centers and list of indices that store the cluster index for each data point
+    """
+    centers = np.zeros((k, data.shape[1]))
+    index = np.zeros(data.shape[0], dtype=int)
+    clusters = [[] for i in range(k)]
+
+    # initialize centers using some random points from data
+    # ....
+    for i in range(data.shape[1]):
+        subset = data[:,i]
+        r = np.random.uniform(
+            np.min(subset),
+            np.max(subset),
+            len(clusters))
+        centers[:,i] = r
+
+    print(centers)
+    convergence = False
+    iterationNo = 0
+    while not convergence:
+        # assign each point to the cluster of closest center
+        # ...
+
+        # update clusters' centers and check for convergence
+        # ...
+
+        iterationNo += 1
+        print('iterationNo = ', iterationNo)
+
+    return index, centers
+
+
+def task_3_a():
+    print("Task 3 (a) ...")
+    img = cv.imread('../images/flower.png')
+    '''
+    ...
+    your code ...
+    ...
+    '''
+    data = img.reshape((img.shape[0] * img.shape[1], 3))
+    myKmeans(data, 5)
+
+def task_3_b():
+    print("Task 3 (b) ...")
+    img = cv.imread('../images/flower.png')
+    '''
+    ...
+    your code ...
+    ...
+    '''
+
+
+def task_3_c():
+    print("Task 3 (c) ...")
+    img = cv.imread('../images/flower.png')
+    '''
+    ...
+    your code ...
+    ...
+    '''
+
+
+##############################################
+#     Task 4        ##########################
+##############################################
+
+
+def task_4_a():
+    print("Task 4 (a) ...")
+    D = None  # construct the D matrix
+    W = None  # construct the W matrix
+    '''
+    ...
+    your code ...
+    ...
+    '''
+
+
+##############################################
+##############################################
+##############################################
+
+if __name__ == "__main__":
+    task_1_a()
+    task_1_b()
+    task_2()
+    task_3_a()
+    task_3_b()
+    task_3_c()
+    task_4_a()
