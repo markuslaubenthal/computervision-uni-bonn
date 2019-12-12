3541bb1b8105384b7b189e866921de4ffce66e1a
Markus Laubenthal <mail@markuslaubenthal.de>
2019-12-12 15:08:44 +0100

    a3t1

--- assignment8/exercise1.py
+++ assignment8/exercise1.py
@@ -60,12 +60,12 @@ def main():
     # Visualize Eigen Faces
     # TODO
     cov_matrix = pca.get_covariance()
-    # fig = plt.figure()
-    # for i, eigenvector in enumerate(pca.components_[:10]):
-    #     ax = plt.subplot(4, 3, i + 1)
-    #     ax.imshow((eigenvector).reshape(h,w), cmap='gray')
-    #
-    # plt.show()
+    fig = plt.figure()
+    for i, eigenvector in enumerate(pca.components_[:10]):
+        ax = plt.subplot(4, 3, i + 1)
+        ax.imshow((eigenvector).reshape(h,w), cmap='gray')
+
+    plt.show()
 
     # Compute reconstruction error
     # TODO
@@ -73,32 +73,26 @@ def main():
         image_pca = pca.transform(images)
         return image_pca
 
+    def calculate_coefficients_for_single_image(image):
+        return pca.transform(image.reshape(1, image.shape[0]))
+
     # X_train_pca, X_test_pca = calculate_coefficients(X_train, X_test)
     eigenvectors = pca.components_
-    coefficients = calculate_coefficients(test_images) / np.linalg.norm(eigenvectors, axis=1) **2
-    print("coefficients", coefficients)
-    print("eigenvectors shape:", pca.components_.shape)
-    print("coefficients shape", coefficients.shape)
-    print(coefficients.dot(eigenvectors).shape)
-    print((pca.mean_[:,None] + eigenvectors.T.dot(coefficients.T)).shape)
+    coefficients = calculate_coefficients(test_images)
+    obj_coefficients = calculate_coefficients(object_images)
+
     images_err = (test_images -
-        (pca.mean_ + coefficients.dot(eigenvectors))
+        (pca.mean_[:,None] + eigenvectors.T.dot(coefficients.T)).T
     )
-    # images_err = (test_images -
-    #     (pca.mean_ + images_pca.dot(eigenvectors) /
-    #     np.linalg.norm(eigenvectors, axis=0) ** 2)
-    # )
+    objects_err = (object_images -
+        (pca.mean_[:,None] + eigenvectors.T.dot(coefficients.T)).T
+    )
+
     error_per_image = np.linalg.norm(images_err, axis = 1)
-    print(error_per_image)
+    error_per_image_obj = np.linalg.norm(objects_err, axis = 1)
 
-    # test = pca.inverse_transform(test_images)
-    # display(test[0].reshape(h,w))
     # display(test_images[0].reshape(h,w))
-
-
-
-    display(test_images[0].reshape(h,w))
-    display((coefficients.dot(eigenvectors))[0].reshape(h,w))
+    # display((pca.mean_[None, :] + coefficients.dot(eigenvectors)) [0].reshape(h,w))
     # display(test_images[1].reshape(h,w))
     # display((pca.mean_ + coefficients.dot(eigenvectors))[1].reshape(h,w))
     # display(test_images[2].reshape(h,w))
@@ -110,9 +104,34 @@ def main():
 
 
     print(error_per_image)
+    print(error_per_image_obj)
     # Perform face detection
     # TODO
+    # Select random image
+    randint = np.random.randint(2)
+    image = None
+    test_label = None
+    if(randint == 0):
+        index = np.random.randint(object_images.shape[0])
+        image = object_images[index]
+        error = error_per_image_obj[index]
+        test_label = 'object'
+    else:
+        index = np.random.randint(test_images.shape[0])
+        image = test_images[index]
+        error = error_per_image[index]
+        test_label = 'face'
 
+    label = None
+    print("The prediction is: ", end='')
+    if(error > 2000):
+        label = "object"
+    else:
+        label = "face"
+    print(label)
+    print("the label is:", test_label)
+
+    display(image.reshape(h,w))
 
     # Perform face recognition
     # TODO
